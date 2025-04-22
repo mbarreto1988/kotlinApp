cda92979fc038ab5811ce8c7a33b5fa099904159
Matias <barretomatiasgabriel@gmail.com>
2025-04-22 20:49:24 -0300

    update main

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -20,9 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             KotlinAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    kotlinAppView(
+                        modifier= Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -30,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinAppTheme {
-        Greeting("Android")
-    }
-}
